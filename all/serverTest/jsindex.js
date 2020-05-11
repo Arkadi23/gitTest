77c3fca299fbe1016fe2cb65f65055670f9b4630
Arkadi23 <62438415+Arkadi23@users.noreply.github.com>
2020-05-11 23:17:33 +0400

    Add testSrv file (js)

--- all/serverTest/jsindex.js
+++ all/serverTest/jsindex.js
@@ -1,17 +1,21 @@
-let apiCall = async (api) => {
-    const apiRoot = 'https://89ef462c-9a42-48f4-b8da-f3a26d60b0e9.mock.pstmn.io';
-    const request = `${apiRoot}${api}`;
-    let response = await fetch(request);
-
-    for (let [key, value] of response.headers) {
-        console.log(`${key} = ${value}`);
+let srvConnection = async (api) => {
+    try {
+        const apiRoot = 'https://58968921-b36d-4b21-b04c-e14d11df0ff5.mock.pstmn.io';
+        const request = `${apiRoot}${api}`;
+        let response = await fetch(request);
+        let json = await response.json();
+        return json;
+    } catch (e) {
+        console.error(e);
     }
 
-    let json = await response.json();
-    return json;
 }
 
-let allCars = apiCall('/cars');
+let allCars = srvConnection('/cars');
 console.log(allCars);
-
-
+let redCar = srvConnection('/cars/red');
+console.log(redCar);
+let greenCar = srvConnection('/cars?color=green');
+console.log(greenCar);
+let blueCar = srvConnection('/cars/blue');
+console.log(blueCar);

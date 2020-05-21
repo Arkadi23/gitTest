49fcf51e38f6c1f246d7ef6c986fb5f5530f6d38
Arkadi23 <62438415+Arkadi23@users.noreply.github.com>
2020-05-21 23:50:27 +0400

    added validetion functions for reg and login forms

--- all/regForm/main.js
+++ all/regForm/main.js
@@ -1,1 +1,31 @@
-console.log('a');
+/*
+    *Forms submit and validation for inputs
+*/
+let getDomELem = (id) => {
+    return document.querySelector(id);
+}
+
+// let checkValue = () => { }
+
+let validation = () => {
+    let email = getDomELem('#email').value;
+    let name = getDomELem('#name').value;
+    let pwd = getDomELem('#pwd').value;
+    if (email.length < 2 || name.length < 2 || pwd.length < 2) {
+        console.log('error');
+        return;
+    }
+    console.log('Ok');
+}
+
+
+getDomELem('#regForm').onsubmit = (event) => {
+    event.preventDefault();
+    validation();
+}
+
+getDomELem('#logForm').onsubmit = (event) => {
+    event.preventDefault();
+    console.log('a');
+}
+
